d73daef21512ec67a63036ee5d105e0b3432e426
yuji91_1227 <yuji91.1227@gmail.com>
2017-09-24 15:42:35 +0000

    add login_function with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
-  resources :pictures, only: %i[index update destroy edit new create]
-  root 'pictures#index'
+  #resources :pictures, only: %i[index update destroy edit new create]
+  #root 'pictures#index'
 end
